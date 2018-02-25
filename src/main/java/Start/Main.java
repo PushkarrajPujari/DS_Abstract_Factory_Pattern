4341ce9be10a61724037716de1977faa91502a78
PushkarrajPujari <pushkarraj@pujari.me>
2018-02-25 15:31:14 +0530

    Stage 1 - Sanity Check

---
+++ src/main/java/Start/Main.java
@@ -0,0 +1,11 @@
+package main.java.Start;
+
+/**
+ * @author Pushkarraj
+ * @since  25-02-2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sanity Check");
+    }
+}
